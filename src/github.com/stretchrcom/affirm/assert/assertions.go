c4aa63971659e02b96110f9de736d24e5190ec61
Tyler Bunnell <tylerbunnell@gmail.com>
2012-10-16 10:33:06 -0600

    Update Assertions to match other repo
    This will be unnecessary once affirm is in its own repo

--- src/github.com/stretchrcom/affirm/assert/assertions.go
+++ src/github.com/stretchrcom/affirm/assert/assertions.go
@@ -16,14 +16,24 @@ import (
 //
 // This function does no assertion of any kind.
 func ObjectsAreEqual(a, b interface{}) bool {
-	return reflect.DeepEqual(a, b)
+
+	if reflect.DeepEqual(a, b) {
+		return true
+	}
+
+	if reflect.ValueOf(a) == reflect.ValueOf(b) {
+		return true
+	}
+
+	return false
+
 }
 
 /* CallerInfo is necessary because the assert functions use the testing object
 internally, causing it to print the file:line of the assert method, rather than where
 the problem actually occured in calling code.*/
 
-// Returns a string containing the file and line number of the assert call
+// CallerInfo returns a string containing the file and line number of the assert call
 // that failed. 
 func CallerInfo() string {
 	_, file, line, ok := runtime.Caller(0)
@@ -41,7 +51,7 @@ func CallerInfo() string {
 		parts = strings.Split(file, "/")
 		dir := parts[len(parts)-2]
 		file = parts[len(parts)-1]
-		if thisDir != dir {
+		if thisDir != dir || file == "assertions_test.go" {
 			break
 		}
 	}
@@ -49,7 +59,7 @@ func CallerInfo() string {
 	return fmt.Sprintf("[ %s:%d ] - ", file, line)
 }
 
-// Asserts that an object is implemented by the specified interface.
+// Implements asserts that an object is implemented by the specified interface.
 //
 // Example
 //    assert.Implements(t, (*core.Codec)(nil), new(JsonCodec), "JsonCodec")
@@ -58,23 +68,23 @@ func Implements(t *testing.T, interfaceObject interface{}, object interface{}, m
 	return True(t, reflect.TypeOf(object).Implements(interfaceType), fmt.Sprintf("%sObject must implement %s. %s", CallerInfo(), interfaceType, message))
 }
 
-// Asserts that the specified object is of the specified type.
+// IsType asserts that the specified object is of the specified type.
 func IsType(t *testing.T, expectedType interface{}, object interface{}, message ...string) bool {
 	return Equal(t, reflect.TypeOf(object), reflect.TypeOf(expectedType), fmt.Sprintf("Object expected to be of type %s, but was %s. %s", reflect.TypeOf(expectedType), reflect.TypeOf(object), message))
 }
 
-// Asserts that two objects are equal.
+// Equal asserts that two objects are equal.
 func Equal(t *testing.T, a, b interface{}, message ...string) bool {
 
 	if !ObjectsAreEqual(a, b) {
-		t.Errorf("%s%s Not equal. %v != %v.", CallerInfo(), message, a, b)
+		t.Errorf("%s%s Not equal. %#v != %#v.", CallerInfo(), message, a, b)
 		return false
 	}
 	return true
 
 }
 
-// Asserts that the specified object is not nil.
+// NotNil asserts that the specified object is not nil.
 func NotNil(t *testing.T, object interface{}, message ...string) bool {
 
 	var success bool = true
@@ -92,7 +102,7 @@ func NotNil(t *testing.T, object interface{}, message ...string) bool {
 	return success
 }
 
-// Asserts that the specified object is nil.
+// Nil asserts that the specified object is nil.
 func Nil(t *testing.T, object interface{}, message ...string) bool {
 
 	if object == nil {
@@ -101,33 +111,33 @@ func Nil(t *testing.T, object interface{}, message ...string) bool {
 		return true
 	}
 
-	t.Errorf("%sExpected to be nil but was %v. %s", CallerInfo(), object, message)
+	t.Errorf("%sExpected to be nil but was %#v. %s", CallerInfo(), object, message)
 
 	return false
 }
 
-// Asserts that the specified value is true.
+// True asserts that the specified value is true.
 func True(t *testing.T, value bool, message ...string) bool {
 	return Equal(t, true, value, message...)
 }
 
-// Asserts that the specified value is true.
+// False asserts that the specified value is true.
 func False(t *testing.T, value bool, message ...string) bool {
 	return Equal(t, false, value, message...)
 }
 
-// Asserts that the specified values are NOT equal.
+// NotEqual asserts that the specified values are NOT equal.
 func NotEqual(t *testing.T, a, b interface{}, message ...string) bool {
 
 	if ObjectsAreEqual(a, b) {
-		t.Errorf("%s%s Should not be equal. %v == %v.", CallerInfo(), message, a, b)
+		t.Errorf("%s%s Should not be equal. %#v == %#v.", CallerInfo(), message, a, b)
 		return false
 	}
 	return true
 
 }
 
-// Asserts that the specified string contains the specified substring.
+// Contains asserts that the specified string contains the specified substring.
 func Contains(t *testing.T, s, contains string, message ...string) bool {
 
 	if !strings.Contains(s, contains) {
@@ -139,7 +149,7 @@ func Contains(t *testing.T, s, contains string, message ...string) bool {
 
 }
 
-// Asserts that the specified string does NOT contain the specified substring.
+// NotContains asserts that the specified string does NOT contain the specified substring.
 func NotContains(t *testing.T, s, contains string, message ...string) bool {
 
 	if strings.Contains(s, contains) {
@@ -151,10 +161,10 @@ func NotContains(t *testing.T, s, contains string, message ...string) bool {
 
 }
 
-// Defines a type that is used for type checking and convenience
+// PanicTestFunc defines a type that is used for type checking and convenience
 type PanicTestFunc func()
 
-// Returns true if the function passed to it panics. Otherwise, it returns false.
+// didPanic returns true if the function passed to it panics. Otherwise, it returns false.
 func didPanic(f PanicTestFunc) bool {
 
 	var didPanic bool = false
@@ -175,12 +185,12 @@ func didPanic(f PanicTestFunc) bool {
 
 }
 
-// Asserts that the function passed to it panics
+// Panics asserts that the function passed to it panics
 func Panics(t *testing.T, f PanicTestFunc, message ...string) bool {
 	return True(t, didPanic(f), fmt.Sprintf("Func should panic but didn't. %s", message))
 }
 
-// Asserts that the function passed to it does not panic
+// NotPanics asserts that the function passed to it does not panic
 func NotPanics(t *testing.T, f PanicTestFunc, message ...string) bool {
 	return False(t, didPanic(f), fmt.Sprintf("Func should not panic. %s", message))
 }
